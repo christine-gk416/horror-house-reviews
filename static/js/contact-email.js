476af988ba13a2a0333832527a99bad97ba3a61c
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-07-30 22:12:06 +0000

    Style and connect contact form in footer The contact form was restyled to fit the design of the site and sized appropriately on mobile views. The EmailJS and SweetAlert CDNs were added to the base template to allow the form to be sent to EmailJS and show alerts. I've updated the contact-email.js file to include the JS used to show these alerts and to send the form to EmailJS

--- static/js/contact-email.js
+++ static/js/contact-email.js
@@ -1,30 +1,69 @@
+//Form Modal//
+
 // Get modal by Id
+window.addEventListener("load", () => {
 
-let modal = document.getElementById("modal");
+  let modal = document.getElementById("modal");
 
-// Get the button that pops out the modal and form
+  // Get the button that pops out the modal and form
 
-let formBtn = document.getElementById("form-btn");
+  let formBtn = document.getElementById("form-btn");
 
-// Get the <span> element that closes the modal
+  // Get the <span> element that closes the modal
 
-let closeForm = document.getElementById("close-form");
+  let closeForm = document.getElementById("close-form");
 
-// Form and modal open when button is clicked on 
+  // Form and modal open when button is clicked on 
 
-formBtn.onclick = function () {
-  modal.style.display = "block";
-};
+  formBtn.onclick = function () {
+    modal.style.display = "block";
+  };
 
-// Form and modal close when <span> element is clicked on
-closeForm.onclick = function () {
-  modal.style.display = "none";
-};
-
-// Form closes if anywhere outside the form is clicked
-
-window.onclick = function (event) {
-  if (event.target == modal) {
+  // Form and modal close when <span> element is clicked on
+  closeForm.onclick = function () {
     modal.style.display = "none";
-  }
-};
+  };
+
+  // Form closes if anywhere outside the form is clicked
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+//Default EmailJS
+
+  // Actions to take when the contact form is submitted
+
+  document.getElementById("contactForm").addEventListener("submit", function (event) {
+
+    // If the event does not get explicitly handled, default action not taken
+
+    event.preventDefault();
+
+    // Send form information to the correct account and using the correct template and default email service
+
+    emailjs.sendForm("gmail", "horror_house", this)
+
+
+    //Sweet Alert
+    
+      // Show Sweet Alert pop-up and post-submit message
+
+      .then(function () {
+        swal("Hello there!", "We've recieved your message and will be back in touch soon", "success", {
+          button: "Thank you!",
+        });
+
+        // Show a console error if form doesn't submit properly
+
+      }, function (error) {
+        swal("Oops", "Something went wrong!", "error")
+        console.log("FAILED...", error);
+      });
+
+    // Reset form after submission   
+
+    document.getElementById("contactForm").reset();
+  });
+});
