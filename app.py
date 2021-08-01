20114663c9d69c28563cd6a69d1d516b667694fc
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-08-01 23:21:50 +0000

    Start connecting MongoDB to the site

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template
-
+if os.path.exists("env.py"):
+    import env
+from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 
 
@@ -26,6 +28,6 @@ def sign_up():
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
         debug=True)
