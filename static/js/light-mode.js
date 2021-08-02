0081391fbfe78ccccb525c42fefdfe6beb56ef41
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-08-02 23:05:32 +0000

    Fix light/dark mode issue on individual review pages Because I'm using two css files to toggle light/dark mode, the individual style sheets weren't being pulled through the individual book review pages. This is because the Java JavaScript file is using static links to the CSS files. To work around this error, I added JavaScript to store the css on the local server. This fixed the error

--- static/js/light-mode.js
+++ static/js/light-mode.js
@@ -1,11 +1,20 @@
-var lightMode = document.querySelector(".btn-toggle");
-var mode = document.querySelector("#mode-link");
+window.onload = function () {
 
- lightMode.addEventListener("click", function() {
- 
-  if (mode.getAttribute("href") == "static/css/dark-mode.css") {
-      mode.href = "static/css/light-mode.css";
+  var lightMode = document.querySelector(".btn-toggle");
+  var mode = document.querySelector("#mode-link");
+  var selected = localStorage.getItem("css");
+    if (selected !== null) {
+      mode.href = selected;
+    } 
+
+
+  lightMode.addEventListener("click", function () {
+
+    if (mode.getAttribute("href") == "/static/css/dark-mode.css") {
+      mode.href = "/static/css/light-mode.css";
     } else {
-    mode.href = "static/css/dark-mode.css";
-  }
-});
+      mode.href = "/static/css/dark-mode.css";
+    }
+    localStorage.setItem("css", mode.href);
+  });
+}
