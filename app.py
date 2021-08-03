eafc4705c27265d37992de5012fb4d69803ef92a
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-08-03 21:11:42 +0000

    Create basic profile page

--- app.py
+++ app.py
@@ -56,6 +56,8 @@ def login():
             existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
         else:
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
@@ -83,8 +85,16 @@ def sign_up():
 
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
 
     return render_template("sign-up.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
