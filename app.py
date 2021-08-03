3d96c73aee426c5ba49394649a972bd1359bbc88
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-08-03 20:00:48 +0000

    Add login functionality

--- app.py
+++ app.py
@@ -48,6 +48,18 @@ def featured(featured_books_id):
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if check_password_hash(
+            existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+        else:
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 
